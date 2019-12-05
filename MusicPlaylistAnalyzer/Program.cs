d1173c6b24220ca1d8814aa50264f9ea43b4bbff
Jack Matasek <jackmatasek@Jacks-MacBook-Pro.local>
2019-12-05 17:04:19 -0600

    Applied .gitignore

--- MusicPlaylistAnalyzer/Program.cs
+++ MusicPlaylistAnalyzer/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace MusicPlaylistAnalyzer
 {
@@ -6,7 +9,91 @@ namespace MusicPlaylistAnalyzer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            if (args.Length != 2)
+            { 
+                Console.WriteLine("MusicPlaylistAnalyzer <music_playlist_file_path> <report_file_path>");
+                Environment.Exit(1);
+            }
+
+            string musicDataFilePath = args[0];
+            string reportFilePath = args[1];
+
+            List<Songs> SongsList = null;
+        }
+    }
+    public class Songs
+    {
+        public string Name;
+        public string Artist;
+        public string Album;
+        public string Genre;
+        public int Size;
+        public int Time;
+        public int Year;
+        public int Plays;
+
+        public Songs(string name, string artist, string album, string genre, int size, int time, int year, int plays)
+        {
+            Name = name;
+            Artist = artist;
+            Album = album;
+            Genre = genre;
+            Size = size;
+            Time = time;
+            Year = year;
+            Plays = plays;
+        }
+    }
+    public static class MusicStatsLoader
+    {
+        private static int NumItemsInRow = 8;
+
+        public static List<Songs> Load(string musicDataFilePath) {
+            List<Songs> SongsList = new List<Songs>();
+
+            try
+            {
+                using (var reader = new StreamReader(musicDataFilePath))
+                {
+                    int lineNumber = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        lineNumber++;
+                        if (lineNumber == 1) continue;
+
+                        var values = line.Split('\t');
+
+                        if (values.Length != NumItemsInRow)
+                        {
+                            throw new Exception($"Row {lineNumber} contains {values.Length} values. It should contain {NumItemsInRow}.");
+                        }
+                        try
+                        {
+                            string name = values[0];
+                            string artist = values[1];
+                            string album = values[2];
+                            string genre = values[3];
+                            int size = Int32.Parse(values[4]);
+                            int time = Int32.Parse(values[5]);
+                            int year = Int32.Parse(values[6]);
+                            int plays = Int32.Parse(values[7]);
+                            Songs songs = new Songs(name, artist, album, genre, size, time, year, plays);
+                            SongsList.Add(songs);
+                        }
+                        catch (FormatException e)
+                        {
+                            throw new Exception($"Row {lineNumber} contains invalid data. ({e.Message})");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Unable to open {musicDataFilePath} ({e.Message}).");
+            }
+
+            return SongsList;
         }
     }
 }
