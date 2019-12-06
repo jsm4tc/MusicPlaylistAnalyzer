2dcebcfc4df276ed175949b661f600f3f2ef6eec
Jack Matasek <jackmatasek@Jacks-MacBook-Pro.local>
2019-12-06 13:08:21 -0600

    finished LINQ queries

--- MusicPlaylistAnalyzer/Program.cs
+++ MusicPlaylistAnalyzer/Program.cs
@@ -131,15 +131,92 @@ namespace MusicPlaylistAnalyzer
             }
 
             report += "Songs that received 200 or more plays: \n";
-            var q1 = from Songs in SongsList where Songs.Plays > 200 select Songs.Name;
+            var q1 = from Songs in SongsList where Songs.Plays > 200 select Songs;
             if (q1.Count() > 0)
             {
                 foreach (var name in q1)
                 {
-                    report += name + ",";
+                    report += "Name: " + name.Name + ", Artist: " + name.Artist + ", Album: " + name.Album + ", Genre: " + name.Genre + ", Size: " + name.Size + ", Time: " + name.Time + ", Year: " + name.Year + ", Plays: " + name.Plays + "\n";
                 }
-                report.TrimEnd(',');
                 report += "\n";
+            }
+
+            int altnum = 0;
+            var q2 = from Songs in SongsList where Songs.Genre == "Alternative" select Songs;
+            if (q2.Count() > 0)
+            {
+                foreach (var alt in q2)
+                {
+                    altnum++;
+                }
+            }
+            report += "Number of Alternative Songs: " + altnum + "\n\n";
+
+            int rapnum = 0;
+            var q3 = from Songs in SongsList where Songs.Genre == "Hip-Hop/Rap" select Songs;
+            if (q3.Count() > 0)
+            {
+                foreach (var rap in q3)
+                {
+                    rapnum++;
+                }
+            }
+            report += "Number of Hip-Hop/Rap songs: " + rapnum + "\n\n";
+
+            report += "Songs from the album Welcome to the Fishbowl: \n";
+            var q4 = from Songs in SongsList where Songs.Album == "Welcome to the Fishbowl" select Songs;
+            if (q4.Count() > 0)
+            {
+                foreach (var fish in q4)
+                {
+                    report += "Name: " + fish.Name + ", Artist: " + fish.Artist + ", Album: " + fish.Album + ", Genre: " + fish.Genre + ", Size: " + fish.Size + ", Time: " + fish.Time + ", Year: " + fish.Year + ", Plays: " + fish.Plays + "\n";
+                }
+                report += "\n";
+            }
+
+            report += "Songs from before 1970: \n";
+            var q5 = from Songs in SongsList where Songs.Year < 1970 select Songs;
+            if (q5.Count() > 0)
+            {
+                foreach (var prior in q5)
+                {
+                    report += "Name: " + prior.Name + ", Artist: " + prior.Artist + ", Album: " + prior.Album + ", Genre: " + prior.Genre + ", Size: " + prior.Size + ", Time: " + prior.Time + ", Year: " + prior.Year + ", Plays: " + prior.Plays + "\n";
+                }
+                report += "\n";
+            }
+
+            report += "Song names longer than 85 characters: \n";
+            var q6 = from Songs in SongsList where Songs.Name.Length > 85 select Songs;
+            if (q6.Count() > 0)
+            {
+                foreach (var big in q6)
+                {
+                    report += big.Name + " ";
+                }
+                report += "\n\n";
+            }
+
+            report += "Longest song: \n";
+            int maxlen = 0;
+            int newlen = 0;
+            var q7 = from Songs in SongsList where Songs.Time > 0 select Songs;
+            if (q7.Count() > 0)
+            {
+                foreach (var len in q7)
+                {
+                    newlen = len.Time;
+                    if (newlen > maxlen)
+                    {
+                        maxlen = newlen;
+                    }
+                }
+                foreach (var sort in q7)
+                {
+                    if (maxlen == sort.Time)
+                    {
+                        report += "Name: " + sort.Name + ", Artist: " + sort.Artist + ", Album: " + sort.Album + ", Genre: " + sort.Genre + ", Size: " + sort.Size + ", Time: " + sort.Time + ", Year: " + sort.Year + ", Plays: " + sort.Plays + "\n";
+                    } 
+                }
             }
 
             return report;
